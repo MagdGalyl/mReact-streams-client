47ca0ed997721d0cda55350bd57028647408b56c
“Magd <majdgalyl@gmail.com>
2022-01-12 05:05:56 +0200

    Added Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import StreamList from "./streams/StreamList";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div>
-      {/* <BrowserRouter>
+      {/* // REACT_ROUTER V5 Syntax {CHANGED IN V6} (Component)=>(ELEMENT plus)
+      <BrowserRouter>
         <div>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" component={StreamCreate} />
@@ -18,8 +20,10 @@ const App = () => {
           <Route path="/streams/delete" component={StreamDelete} />
           <Route path="/streams/show" component={StreamShow} />
         </div>
-      </BrowserRouter> */}
+      </BrowserRouter> 
+      */}
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" exact element={<StreamList />} />
           <Route path="/streams/new" element={<StreamCreate />} />
