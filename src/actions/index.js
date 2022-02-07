e62b325bd769a197bcad0cf06f4e1a0bd2e03b7e
“Magd <majdgalyl@gmail.com>
2022-02-07 03:02:13 +0200

    React Router V6 meh again

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import streams from "../apis/streams";
->>>>>>> 8348d320a75fdd5c013556859ca1c4ced655ec4f
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -11,10 +8,6 @@ import {
   DELETE_STREAM,
   EDIT_STREAM,
 } from "./types";
-<<<<<<< HEAD
-import streams from "../apis/streams";
-=======
->>>>>>> 8348d320a75fdd5c013556859ca1c4ced655ec4f
 
 export const signIn = (userId) => {
   return {
@@ -29,46 +22,6 @@ export const signOut = () => {
   };
 };
 
-<<<<<<< HEAD
-export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await streams.post("/streams", formValues);
-
-    dispatch({ type: CREATE_STREAM, payload: response.data });
-  };
-};
-
-export const fetchStreams = () => {
-  return async (dispatch) => {
-    const response = streams.get("/streams");
-
-    dispatch({ type: FETCH_STREAMS, payload: response.data });
-  };
-};
-
-export const fetchStream = (id) => {
-  return async (dispatch) => {
-    const response = await streams.get(`/streams/${id}`);
-
-    dispatch({ type: FETCH_STREAM, payload: response.data });
-  };
-};
-
-export const editStream = (id, formValues) => {
-  return async (dispatch) => {
-    const response = await streams.put(`/streams/${id}`, formValues);
-
-    dispatch({ type: EDIT_STREAM, payload: response.data });
-  };
-};
-
-export const deleteStream = (id) => {
-  return async (dispatch) => {
-    await streams.delete(`/streams/${id}`);
-
-    dispatch({ type: DELETE_STREAM, payload: id });
-  };
-=======
 export const createStream =
   (formValues, userId) => async (dispatch, getState) => {
     const { userId } = getState().auth;
@@ -99,5 +52,4 @@ export const deleteStream = (id) => async (dispatch) => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload: id });
->>>>>>> 8348d320a75fdd5c013556859ca1c4ced655ec4f
 };
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
 export const CREATE_STREAM = "CREATE_STREAM";
@@ -6,12 +5,3 @@ export const FETCH_STREAMS = "FETCH_STREAMS";
 export const FETCH_STREAM = "FETCH_STREAM";
 export const DELETE_STREAM = "DELETE_STREAM";
 export const EDIT_STREAM = "EDIT_STREAM";
-=======
-export const SIGN_IN = 'SIGN_IN';
-export const SIGN_OUT = 'SIGN_OUT';
-export const CREATE_STREAM = 'CREATE_STREAM';
-export const FETCH_STREAMS = 'FETCH_STREAMS';
-export const FETCH_STREAM = 'FETCH_STREAM';
-export const DELETE_STREAM = 'DELETE_STREAM';
-export const EDIT_STREAM = 'EDIT_STREAM';
->>>>>>> 8348d320a75fdd5c013556859ca1c4ced655ec4f
